22fa1dacebed7576e3b3654b56e8eb4a30a74a69
AlejandroElias1 <aleeli889@alu.centroafuera.es>
2022-11-17 11:48:06 +0100

    LayOut Calculadora Terminada

---
+++ app/src/main/java/com/example/task07_layouts2/Calculadora.kt
@@ -0,0 +1,11 @@
+package com.example.task07_layouts2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calculadora : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora)
+    }
+}
